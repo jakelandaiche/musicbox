a54e0dab2ad6da9a9522008f194827be9cd5e281
jakelandaiche <jakelandaiche@gmail.com>
2024-02-04 22:47:31 -0500

    gen random colors and set word colors

--- src/host/view.js
+++ src/host/view.js
@@ -1,69 +1,77 @@
-import { socket } from "../socket.js"
-import { bind } from "../state.js"
-import {
-  STATE,
-  CODE,
-  PLAYERS,
-  PLAYER_DATA,
-  ROUND_NUM,
-} from "./model.js"
+import { socket } from "../socket.js";
+import { bind } from "../state.js";
+import { STATE, CODE, PLAYERS, PLAYER_DATA, ROUND_NUM } from "./model.js";
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var colors = [];
+for (var x = 0; x < 25; x++) {
+  colors.push(getRandomColor());
+}
 
 export const stateViews = {
   CONNECT: {
     div: "div-connect",
     init: () => {
-      const connectform = document.getElementById("connectform")
+      const connectform = document.getElementById("connectform");
       connectform.addEventListener("submit", (event) => {
-        event.preventDefault() // prevent browser from reloading (default behavior)
+        event.preventDefault(); // prevent browser from reloading (default behavior)
 
         // init socket
-        const formData = new FormData(connectform)
-        const ws_host = formData.get("ws-host")
-        socket.init(ws_host)
-      })
+        const formData = new FormData(connectform);
+        const ws_host = formData.get("ws-host");
+        socket.init(ws_host);
+      });
     },
     reset: () => {
-      const hostinput = document.getElementById("connectform-hostinput")
-      hostinput.innerText = "ws://localhost:8080"
+      const hostinput = document.getElementById("connectform-hostinput");
+      hostinput.innerText = "ws://localhost:8080";
     },
   },
 
   LOBBY: {
     div: "div-lobby",
     init: () => {
-      const codetext = document.getElementById("lobby-codetext")
-      const playerlist = document.getElementById("lobby-playerlist")
-      const startbtn = document.getElementById("lobby-startbtn")
+      const codetext = document.getElementById("lobby-codetext");
+      const playerlist = document.getElementById("lobby-playerlist");
+      const startbtn = document.getElementById("lobby-startbtn");
 
       startbtn.addEventListener("click", (event) => {
         socket.send({
           type: "start",
-        })
-      })
+        });
+      });
 
       bind(CODE, (c) => {
-        codetext.innerText = c
-      })
+        codetext.innerText = c;
+      });
 
       bind(PLAYERS, (p) => {
         while (playerlist.hasChildNodes())
-          playerlist.removeChild(playerlist.firstChild)
+          playerlist.removeChild(playerlist.firstChild);
 
         p.forEach((player) => {
-          const entry = document.createElement("p")
-          entry.innerText = player.name + (player.ready ? " ✅" : "")
-          entry.style.color = player.color
-          playerlist.appendChild(entry)
-        })
-      })
+          const entry = document.createElement("p");
+          entry.innerText = player.name + (player.ready ? " ✅" : "");
+          entry.style.color = player.color;
+          playerlist.appendChild(entry);
+        });
+      });
     },
     reset: () => {
-      const codetext = document.getElementById("lobby-codetext")
-      const playerlist = document.getElementById("lobby-playerlist")
+      const codetext = document.getElementById("lobby-codetext");
+      const playerlist = document.getElementById("lobby-playerlist");
 
-      codetext.innerText = ""
+      codetext.innerText = "";
       while (playerlist.hasChildNodes())
-        playerlist.removeChild(playerlist.firstChild)
+        playerlist.removeChild(playerlist.firstChild);
     },
   },
 
@@ -76,11 +84,11 @@ export const stateViews = {
   ROUNDSTART: {
     div: "div-roundstart",
     init: () => {
-      const roundnumtext = document.getElementById("roundstart-roundnumtext")
+      const roundnumtext = document.getElementById("roundstart-roundnumtext");
 
       bind(ROUND_NUM, (round_num) => {
-        roundnumtext.innerText = `Round ${round_num}`
-      })
+        roundnumtext.innerText = `Round ${round_num}`;
+      });
     },
     reset: () => {},
   },
@@ -88,34 +96,34 @@ export const stateViews = {
   ROUNDCOLLECT: {
     div: "div-roundcollect",
     init: () => {
-      const playerlist = document.getElementById("roundcollect-playerlist")
+      const playerlist = document.getElementById("roundcollect-playerlist");
 
       bind(PLAYER_DATA, (player_data) => {
         while (playerlist.hasChildNodes())
-          playerlist.removeChild(playerlist.firstChild)
+          playerlist.removeChild(playerlist.firstChild);
 
         player_data
           .map((data) => {
-            const d = document.createElement("div")
-            const p = document.createElement("p")
+            const d = document.createElement("div");
+            const p = document.createElement("p");
             p.innerText =
               data.name +
               (data.answer !== null ? " ✅" : "") +
-              ` (${data.total})`
+              ` (${data.total})`;
 
-            p.style.color = data.color
+            p.style.color = data.color;
 
-            const p2 = document.createElement("p")
+            const p2 = document.createElement("p");
             p2.innerText =
-              data.answer !== null ? "*".repeat(data.answer.length) : ""
+              data.answer !== null ? "*".repeat(data.answer.length) : "";
 
-            d.appendChild(p)
-            d.appendChild(p2)
+            d.appendChild(p);
+            d.appendChild(p2);
 
-            return d
+            return d;
           })
-          .forEach((elem) => playerlist.appendChild(elem))
-      })
+          .forEach((elem) => playerlist.appendChild(elem));
+      });
     },
     reset: () => {},
   },
@@ -123,36 +131,55 @@ export const stateViews = {
   ROUNDEND: {
     div: "div-roundend",
     init: () => {
-      const playerlist = document.getElementById("roundend-playerlist")
+      const playerlist = document.getElementById("roundend-playerlist");
 
       bind(PLAYER_DATA, (player_data) => {
         while (playerlist.hasChildNodes())
-          playerlist.removeChild(playerlist.firstChild)
+          playerlist.removeChild(playerlist.firstChild);
 
         player_data
           .map((data) => {
-            const d = document.createElement("div")
-            const p = document.createElement("p")
+            const d = document.createElement("div");
+            const p = document.createElement("p");
             p.innerText =
               data.name +
               (data.answer !== null ? " ✅" : "") +
-              ` (${data.total})`
-            p.style.color = data.color
+              ` (${data.total})`;
+            p.style.color = data.color;
 
-            const p2 = document.createElement("p")
-            p2.innerText = data.answer !== null ? data.answer : ""
+            const p2 = document.createElement("p");
+            if (data.answer !== null) {
+              var answer_colors = data.color_list;
+              var answer_words = data.answer.split(" ");
+              for (var i = 0; i < answer_colors.length; i++) {
+                if (answer_colors[i] != 0) {
+                  styled_word =
+                    "<span style='color: " +
+                    colors[answer_colors[i]] +
+                    ";'>" +
+                    answer_words[i] +
+                    "</span> ";
+                  p2.innerText.concat(styled_word);
+                }
+                else {
+                  p2.innerText.concat(answer_words[i] + " ");
+                }
+              }
+            } else {
+              p2.innerText = "";
+            }
 
-            const p3 = document.createElement("p")
-            p3.innerText = `Score: ${data.score}`
+            const p3 = document.createElement("p");
+            p3.innerText = `Score: ${data.score}`;
 
-            d.appendChild(p)
-            d.appendChild(p2)
-            d.appendChild(p3)
+            d.appendChild(p);
+            d.appendChild(p2);
+            d.appendChild(p3);
 
-            return d
+            return d;
           })
-          .forEach((elem) => playerlist.appendChild(elem))
-      })
+          .forEach((elem) => playerlist.appendChild(elem));
+      });
     },
     reset: () => {},
   },
@@ -160,33 +187,33 @@ export const stateViews = {
   GAMEEND: {
     div: "div-gameend",
     init: () => {
-      const playerlist = document.getElementById("gameend-playerlist")
+      const playerlist = document.getElementById("gameend-playerlist");
 
       bind(PLAYER_DATA, (player_data) => {
         while (playerlist.hasChildNodes())
-          playerlist.removeChild(playerlist.firstChild)
+          playerlist.removeChild(playerlist.firstChild);
 
         player_data
           .map((data) => {
-            const d = document.createElement("div")
-            const p = document.createElement("p")
-            p.innerText = data.name
-            p.style.color = data.color
+            const d = document.createElement("div");
+            const p = document.createElement("p");
+            p.innerText = data.name;
+            p.style.color = data.color;
 
-            const p2 = document.createElement("p")
-            p2.innerText = `Final score: ${data.total}`
+            const p2 = document.createElement("p");
+            p2.innerText = `Final score: ${data.total}`;
 
-            d.appendChild(p)
-            d.appendChild(p2)
+            d.appendChild(p);
+            d.appendChild(p2);
 
-            return d
+            return d;
           })
-          .forEach((elem) => playerlist.appendChild(elem))
-      })
+          .forEach((elem) => playerlist.appendChild(elem));
+      });
     },
     reset: () => {},
   },
-}
+};
 
 const divs = {
   CONNECT: "div-connect",
@@ -196,22 +223,22 @@ const divs = {
   ROUNDCOLLECT: "div-roundcollect",
   ROUNDEND: "div-roundend",
   GAMEEND: "div-gameend",
-}
+};
 
 bind(STATE, (s) => {
   Object.keys(stateViews).forEach((k) => {
-    document.getElementById(stateViews[k].div).style.display = "none"
-  })
-  Object.keys(stateViews).forEach((k) => stateViews[k].reset())
-  document.getElementById(stateViews[s].div).style.display = "block"
-})
+    document.getElementById(stateViews[k].div).style.display = "none";
+  });
+  Object.keys(stateViews).forEach((k) => stateViews[k].reset());
+  document.getElementById(stateViews[s].div).style.display = "block";
+});
 
 export function initViews() {
-  Object.keys(stateViews).forEach((k) => stateViews[k].init())
+  Object.keys(stateViews).forEach((k) => stateViews[k].init());
 }
 
 export function initVideoPlayer() {
   socket.addMessageHandler("video", (message) => {
-    play_video(message.id, message.start_time, message.start_time + 10)
-  })
+    play_video(message.id, message.start_time, message.start_time + 10);
+  });
 }
