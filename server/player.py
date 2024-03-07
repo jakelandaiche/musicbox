549e8dfdabb43f0ff09dfd7a2d3a160b1294900c
Jasper Ty <jasperty2@gmail.com>
2024-03-07 15:21:05 -0500

    minor change to player.py field syntax

--- server/player.py
+++ server/player.py
@@ -20,10 +20,10 @@ class Player:
     score: int = 0
     total: int = 0
     db_id: str | None = None
-    score_info = dict()
-    unique_words = set()
-    word_count = 0
-    word_len = 0
+    score_info: dict = field(default_factory=dict)
+    unique_words: set = field(default_factory=set)
+    word_count: int = 0
+    word_len: int = 0
 
     @property
     def connected(self):
@@ -50,7 +50,3 @@ class Player:
             "avg_len": self.word_len / self.word_count if self.word_count > 0 else 0,
         }
 
-
-class PlayerManager:
-    def __init__(self):
-        pass
